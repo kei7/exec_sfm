9d21d564388cce8f0834e36547885c83dc0653ab
kei7 <kei950120@gmail.com>
2019-11-20 15:49:11 +0000

    validation for sfm and optical and 3dprint model

---
+++ 20191120-1/exec_opt_texture_new.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#optical texture for model
+. ./setup.txt
+
+#cp -r /home/repos/install/install/${IMGDIRNAME}/opt ${MVGBUILDMAIN}/${MVGEXEC}/input/${IMGDIRNAME}/opt
+python opt_list.py /home/repos ${IMGDIRNAME}
+
+mkdir /home/repos/install/install/${IMGDIRNAME}/opt_out
+
+cat opt_list_new.txt | while read line
+do
+  cp ${MVGBUILDMAIN}/${MVGEXEC}/input/${IMGDIRNAME}/opt/$line.jpg ${MVGBUILDMAIN}/${MVGEXEC}/input/${IMGDIRNAME}/images/OPTA0000001.jpg
+  cp ${MVGBUILDMAIN}/${MVGEXEC}/input/${IMGDIRNAME}/opt/$line.jpg ./undistorted_images/OPTA0000001.jpg
+  cp ${MVGBUILDMAIN}/${MVGEXEC}/input/${IMGDIRNAME}/opt/$line.jpg ${MVSBUILDMAIN}/undistorted_images/${IMGDIRNAME}/images/OPTA0000001.jpg
+  ${MVSBUILDMAIN}/TextureMesh ${MVGBUILDMAIN}/${MVGEXEC}/input/${IMGDIRNAME}/out/reconstruction_sequential/scene_dense_mesh.mvs --mesh-file ${MVGBUILDMAIN}/${MVGEXEC}/input/${IMGDIRNAME}/out/reconstruction_sequential/scene_dense_mesh_del.ply --re-texture 1
+  mv ${MVGBUILDMAIN}/${MVGEXEC}/input/${IMGDIRNAME}/out/reconstruction_sequential/scene_dense_mesh_texture.ply /home/repos/install/install/${IMGDIRNAME}/opt_out/scene_dense_mesh_texture_$line.ply
+  mv ${MVGBUILDMAIN}/${MVGEXEC}/input/${IMGDIRNAME}/out/reconstruction_sequential/scene_dense_mesh_texture.png /home/repos/install/install/${IMGDIRNAME}/opt_out/scene_dense_mesh_texture_$line.png
+done 
---
+++ 20191120-2/clean_prev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+. ./setup.txt
+rm -r /home/repos/openMVG_build/software/SfM/input/${IMGDIRNAME}
